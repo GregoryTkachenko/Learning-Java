abc3bc67cfff9b4691185f9c1b6245a7ed38ed08
GregoryTkachenko <86478863+GregoryTkachenko@users.noreply.github.com>
2021-09-08 12:48:51 +0300

    Update ArrayLength.java
    Add new element

--- src/com/company/JavaArrays/ArrayLength.java
+++ src/com/company/JavaArrays/ArrayLength.java
@@ -4,7 +4,7 @@ To find out how many elements an array has, use the length property:
  */
 public class ArrayLength {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "Nisan"};
         System.out.println(cars.length);        // Outputs 4
     }
 }
